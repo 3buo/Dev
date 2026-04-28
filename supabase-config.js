f008ad96d6cf64ad6763c4f577dd79ac50937941
3buo <116233931+3buo@users.noreply.github.com>
2026-04-27 22:34:58 -0400

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,2 +0,0 @@
-export const supabaseUrl = 'https://snruccregkwcsnptojvw.supabase.co';
-export const supabaseKey = 'sb_publishable_c-NOpMRqd0E2P-QW3IEfOw_MHRuq9FO'; // usa la que dice "Copy publishable key"
