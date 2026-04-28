7fdc0b1ca3bf8914cd0fc28084168406ca8dbdea
3buo <116233931+3buo@users.noreply.github.com>
2026-04-27 21:51:53 -0400

    Delete backend directory

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-
-export const SUPABASE_URL = "https://snruccregkwcsnptojvw.supabase.co";
-
-// 🔐 NUNCA EXPONER
-export const SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNucnVjY3JlZ2t3Y3NucHRvanZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjE5MDUsImV4cCI6MjA5Mjg5NzkwNX0.1Tl9DKHx8LMrHisaNvktl9fW3P0KRJnlVdQH3pdEMYo";
-
-export const PORT = 3000;
