28d0d087c676a7ee5117b813259262b319c67a30
3buo <3buotv@gmail.com>
2026-05-20 00:19:18 -0400

    `Deleted all files in the dev-builder project`

--- dev-builder/.DEV/components/devmode/legacy-devmode.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '../../../../components/devmode/devmode.js' {
-  export function initDevMode(): void
-}
--- dev-builder/src/devmode/serialize.ts
+++
@@ -1,9 +0,0 @@
-import type { BuilderTree, SerializeResult } from './types'
-
-export function serializeTree(tree: BuilderTree): SerializeResult {
-  return {
-    version: 1,
-    tree,
-  }
-}
-
